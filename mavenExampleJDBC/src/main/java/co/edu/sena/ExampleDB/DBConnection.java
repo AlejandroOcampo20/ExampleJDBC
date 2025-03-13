e93f12a29fce6d461a71fc4aeb77ea56467d65fb
Alejandro-ocampo <alejosaya@gmail.com>
2025-03-13 11:49:00 -0500

    DBemployeeType
    Se codifica la conexion a la tabla employee_Type con sus respectivos tests.

--- mavenExampleJDBC/src/main/java/co/edu/sena/ExampleDB/DBConnection.java
+++ mavenExampleJDBC/src/main/java/co/edu/sena/ExampleDB/DBConnection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package co.edu.sena.ExampleJDBC;
+package co.edu.sena.ExampleDB;
 
 import co.edu.sena.ExampleJDBC.Utils.MessageUtils;
 import java.sql.Connection;
