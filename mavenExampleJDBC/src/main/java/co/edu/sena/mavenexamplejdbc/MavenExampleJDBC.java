add3b15bb64a321c985281301773089197db52e2
Alejandro-ocampo <alejosaya@gmail.com>
2025-03-13 08:10:34 -0500

    Configuración proyecto
    Se añade dependencia de MYSQL conecctorj al
    pom y se agregan tags de documentación

---
+++ mavenExampleJDBC/src/main/java/co/edu/sena/mavenexamplejdbc/MavenExampleJDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.sena.mavenexamplejdbc;
+
+/**
+ *
+ * @author alejo
+ */
+public class MavenExampleJDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
