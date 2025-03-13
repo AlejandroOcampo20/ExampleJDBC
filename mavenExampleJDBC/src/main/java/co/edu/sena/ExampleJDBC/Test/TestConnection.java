a7b3f90bad4e471e2b291fcc96d307bc1b41bc9b
Alejandro-ocampo <alejosaya@gmail.com>
2025-03-13 09:10:38 -0500

    Conexion a JDBC
    Se crea la conexion a MYSQL. tambien se agrega la clase para manejarla

---
+++ mavenExampleJDBC/src/main/java/co/edu/sena/ExampleJDBC/Test/TestConnection.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.sena.ExampleJDBC.Test;
+
+import co.edu.sena.ExampleJDBC.DBConnection;
+
+/**
+ * 13/03/2025
+ * @author alejo
+ * Objetive: probar a conexio a la base de datos
+ */
+public class TestConnection {
+    public static void main(String[] args) {
+        DBConnection connection = new DBConnection();
+        connection.connect();
+        connection.disconnect();
+    }
+}
